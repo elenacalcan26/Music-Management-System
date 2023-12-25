71f7c183ac1e6a396f2a8c382a6ef27d59cf5c4f
elenacalcan26 <elena.calcan26@gmail.com>
2023-12-25 13:17:31 +0200

    add NoItemPresentInTable in main method signature

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,9 +1,10 @@
 package main;
 
+import exceptions.NoItemPresentInTable;
 import parser.JsonParser;
 
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws NoItemPresentInTable {
     JsonParser.parseInput(args[0]);
   }
 }
